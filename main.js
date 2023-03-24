fb35eaa7178af0f3db22a52894d8392680157794
Diego Martinez <martinez.diego90@gmail.com>
2023-03-24 22:20:40 +0100

    adding filters

--- main.js
+++ main.js
@@ -1,27 +1,55 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            urlApi: 'https://mindhub-xj03.onrender.com/api/amazing',
-            events: [],
+  data() {
+    return {
+      urlApi: "https://mindhub-xj03.onrender.com/api/amazing",
+      events: [],
+      eventsFiltered: [],
+      searchText: "",
+      categories: [],
+      categoryEvents: [],
+    };
+  },
+  created() {
+    this.fetchingData();
+  },
+  mounted() {},
+  methods: {
+    fetchingData() {
+      fetch(this.urlApi)
+        .then((response) => response.json())
+        .then((data) => {
+          events = data.events;
+          this.events = events;
+          console.log(events);
+          this.eventsFiltered = this.events;
+          this.fetchingCategories(events);
+        })
+        .catch((error) => console.log(error.message));
+    },
+    fetchingCategories(array) {
+      array.forEach((element) => {
+        if (!this.categories.includes(element.category)) {
+          this.categories.push(element.category);
         }
+      });
     },
-    created() {
-        this.fetchingData()
+  },
+  computed: {
+    searchFilter() {
+      this.events = this.eventsFiltered.filter((event) =>
+        event.name.toLowerCase().includes(this.searchText.toLowerCase())
+      );
     },
-    mounted() { },
-    methods: {
-        fetchingData() {
-            fetch(this.urlApi)
-                .then((response) => response.json())
-                .then((apiData) => {
-                    console.log(apiData);
-                })
-            .catch(error => console.log(error.message))
-        },
+    categoryFilter() {
+      if (this.categoryEvents.length > 0) {
+        this.events = this.eventsFiltered.filter((event) =>
+          this.categoryEvents.includes(event.category)
+        );
+      } else {
+        this.events = this.eventsFiltered;
+      }
     },
-    computed: {
-        
-    }
-}).mount('#app')
+  },
+}).mount("#app");
