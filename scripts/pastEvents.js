7779fc9026d4ba958ed2a3964e915fd51b899f14
Diego Martinez <martinez.diego90@gmail.com>
2023-04-18 21:51:13 +0200

    past events cards showed

--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -1,58 +1,44 @@
 // past events
-this.pastEvents = this.events.filter(
-    (item) => item.date < data.currentDate
-  );
-  this.filterPastEvents = [...this.pastEvents];
-  this.pastCategories = [
-    ...new Set(this.pastEvents.map((element) => element.category)),
-  ];
-  // upcoming events
-  this.upcomingEvents = this.events.filter(
-    (item) => item.date > data.currentDate
-  );
-  this.filterUpcomingEvents = [...this.upcomingEvents];
-  this.upcomingCategories = [
-    ...new Set(this.upcomingEvents.map((element) => element.category)),
-];
-  
-const {createApp} = Vue;
+// this.pastEvents = this.events.filter((item) => item.date < data.currentDate);
+// this.filterPastEvents = [...this.pastEvents];
+// this.pastCategories = [
+//   ...new Set(this.pastEvents.map((element) => element.category)),
+// ];
+
+const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
+  data() {
+    return {
       urlApi: "https://mindhub-xj03.onrender.com/api/amazing",
+      events: [],
+      pastEvents: [],
+      searchText: "",
       categories: [],
-      checked:[],
-      valueOfSearch:"",
-      filterEvents:[]
-    }
+      categoryEvents: [],
+      searchText: "",
+      filterEvents: [],
+    };
   },
   created() {
     this.fetchingData();
-    // fetch('https://mindhub-xj03.onrender.com/api/amazing')
-    //   .then(res => res.json())
-    //   .then(res =>{
-    //     this.apiInfo = res.events.filter(item => item.date < res.currentDate)
-    //     this.filterEvents =[...this.apiInfo] 
-    //     this.categories = [ ...new Set( this.apiInfo.map( element => element.category ) ) ]
-    //   }
-        
-        
-    //   ).catch(err=>console.log(err))
-  }, 
+  },
   methods: {
     fetchingData() {
       fetch(this.urlApi)
         .then((response) => response.json())
         .then((data) => {
           events = data.events;
-          this.pastEvents = this.events.filter((item) => item.date < data.currentDate);
-          this.filterPastEvents = [...this.pastEvents];
-          this.pastCategories = [...new Set(this.pastEvents.map((element) => element.category)),
-          ];
-        }
-      },  
-fetchingCategories(array) {
+          this.events = events;
+          this.pastEvents = this.events.filter(
+            (item) => item.date < data.currentDate
+          );
+          console.log(pastEvents);
+          this.fetchingCategories(pastEvents);
+        })
+        .catch((error) => console.log(error.message));
+    },
+    fetchingCategories(array) {
       array.forEach((element) => {
         if (!this.categories.includes(element.category)) {
           this.categories.push(element.category);
@@ -61,16 +47,8 @@ fetchingCategories(array) {
     },
   },
   computed: {
-  //   crossFilter: function(){
-  //     let filterBySearch = this.apiInfo.filter( event => event.name.toLowerCase().includes( this.valueOfSearch.toLowerCase()))
-  //     if( this.checked.length === 0 ){
-  //         this.filterEvents = filterBySearch
-  //     }else{
-  //         let filterByCheck = filterBySearch.filter( event => this.checked.includes( event.category ))
-  //         this.filterEvents = filterByCheck 
-  // } 
-checkboxAndBarFilter() {
-      let searchFilter = this.eventsFiltered.filter((event) =>
+    checkboxAndBarFilter() {
+      let searchFilter = this.pastEvents.filter((event) =>
         event.name.toLowerCase().includes(this.searchText.toLowerCase())
       );
       if (this.categoryEvents.length > 0) {
@@ -81,5 +59,5 @@ checkboxAndBarFilter() {
         this.events = searchFilter;
       }
     },
-  }
-}).mount("#app")
+  },
+}).mount("#app");
